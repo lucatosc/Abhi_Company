a17fac560aaff8f944e9adc5d9262089eba0d27a
lucatosc <toscano6210@gmail.com>
2024-11-27 04:57:24 +0100

    Add .gif file

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,60 +1,12 @@
-import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import TrackVisibility from 'react-on-screen';
-// import RotatingGlobe from "../assets/img/RotatingEarth.gif";
-import Blob from './Blob'
-import Slider from "./Slider";
-// import Slider from  './sliderv2'
+import RotatingGlobe from "../../assets/img/RotatingEarth.gif";
 import '../../App.css'
 import './GlitchEffect.css'
 
 
 export const Banner = () => {
-  const [loopNum, setLoopNum] = useState(0);
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [text, setText] = useState('');
-  const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const [index, setIndex] = useState(1);
-  const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
-  const period = 2000;
-  
-
-  useEffect(() => {
-    let ticker = setInterval(() => {
-      tick();
-    }, delta);
-
-    return () => {
-       clearInterval(ticker) 
-      };
-    },[text]);
-
-  const tick = () => {
-    let i = loopNum % toRotate.length;
-    let fullText = toRotate[i];
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
-    setText(updatedText);
-
-    if (isDeleting) {
-      setDelta(prevDelta => prevDelta / 2);
-    }
-
-    if (!isDeleting && updatedText === fullText) {
-      setIsDeleting(true);
-      setIndex(prevIndex => prevIndex - 1);
-      setDelta(period);
-    } else if (isDeleting && updatedText === '') {
-      setIsDeleting(false);
-      setLoopNum(loopNum + 1);
-      setIndex(1);
-      setDelta(500);
-    } else {
-      setIndex(prevIndex => prevIndex + 1);
-    }
-  }
-
   return (
     <section className="banner" id="home">
       <Container>
@@ -74,6 +26,11 @@ export const Banner = () => {
             </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={5}>
+            <div className="flex justify-end">
+              <div className="w-80 h-80">
+                <img src={RotatingGlobe} alt="IMAGE-EARTH" />
+              </div>
+            </div>
           </Col>
         </Row>
       </Container>
